99d8be1c70ae008594d7abddfec71e3a383467e6
Ji-Rath <garethaguilar@gmail.com>
2021-10-15 16:03:47 -0500

    Adjust PlayerPushComponent class
    Add additional comments in .h and .cpp file
    Fix potential crash when PushObject is called with nullptr PushComponent
    Change GrabComponentAtLocation to GrabComponentAtLocationWithRotation
    Replace some hard coded values with adjustable variables

--- Plugins/UEInteractSystem/Source/InteractionSystem/Private/Interaction/PlayerPushComponent.cpp
+++ Plugins/UEInteractSystem/Source/InteractionSystem/Private/Interaction/PlayerPushComponent.cpp
@@ -3,6 +3,7 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "Interaction/ToggleInteractable.h"
+#include "DrawDebugHelpers.h"
 
 
 UPlayerPushComponent::UPlayerPushComponent()
@@ -18,9 +19,11 @@ void UPlayerPushComponent::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	/** Get Character Movement Component from owner */
 	CharacterMoveComp = GetOwner()->FindComponentByClass<UCharacterMovementComponent>();
 	ensureMsgf(CharacterMoveComp, TEXT("No Character Movement Component found for %s"), *(GetOwner()->GetName()));
 
+	/** Get Physics Handle Component from owner */
 	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
 	ensureMsgf(CharacterMoveComp, TEXT("No Physics Handle Component found for %s"), *(GetOwner()->GetName()));
 }
@@ -31,33 +34,52 @@ void UPlayerPushComponent::TickComponent(float DeltaTime, enum ELevelTick TickTy
 	
 	if (bPushingObject && PhysicsHandle->GetGrabbedComponent())
 	{
-		FVector TargetLocation = TargetOffset + GetOwner()->GetActorLocation();
-		PhysicsHandle->SetTargetLocation(TargetLocation);
+		FVector OwnerLocation = GetOwner()->GetActorLocation();
+		FVector TargetLocation = TargetOffset + OwnerLocation;
 
-		if (FVector::Distance(TargetLocation, GetOwner()->GetActorLocation()) > (TargetOffset.Size() + 25.f))
+		/** Ensure player does not get too far from TargetLocation */
+		FHitResult OutHit;
+		GetWorld()->LineTraceSingleByChannel(OutHit, OwnerLocation, OwnerLocation + (UKismetMathLibrary::GetForwardVector(TargetRotation) * MaxObjectDistance), ECC_Visibility);
+
+		/* Stop pushing if the object is no longer infront of the player */
+		if (OutHit.GetComponent() != PhysicsHandle->GetGrabbedComponent())
 		{
 			AToggleInteractable* PushBox = Cast<AToggleInteractable>(PhysicsHandle->GetGrabbedComponent()->GetOwner());
 			PushBox->SetState(GetOwner(), false);
 		}
+
+		/* Push object away if its too close, fixes slow movement while pushing */
+		if (FVector::Distance(OutHit.ImpactPoint, OwnerLocation) < MinObjectDistance)
+		{
+			TargetLocation += UKismetMathLibrary::GetForwardVector(TargetRotation) * (MinObjectDistance - FVector::Distance(OutHit.ImpactPoint, OwnerLocation));
+		}
+
+		/** Update physics handle target location */
+		PhysicsHandle->SetTargetLocation(TargetLocation);
 	}
 }
 
 void UPlayerPushComponent::PushObject(UPrimitiveComponent* PushComponent)
 {
-	if (!CharacterMoveComp || !PhysicsHandle) { return; }
+	if (!CharacterMoveComp || !PhysicsHandle || !PushComponent) { return; }
 	
 	bPushingObject = true;
 
-	CharacterMoveComp->MaxWalkSpeed = 200.f;
+	CharacterMoveComp->MaxWalkSpeed = PushMaxSpeed;
 	CharacterMoveComp->bOrientRotationToMovement = false;
 
+	/* Draw a line from owner to pushed object center of mass */
 	FHitResult OutHit;
 	GetWorld()->LineTraceSingleByChannel(OUT OutHit, GetOwner()->GetActorLocation(), PushComponent->GetCenterOfMass(), ECC_Visibility);
 
+	/* Sets rotation to face pushed object relative to player */
 	TargetRotation = UKismetMathLibrary::MakeRotFromX(OutHit.ImpactNormal * -1.f);
+
+	/* Pushed object offset relative to player */
 	TargetOffset = OutHit.ImpactPoint - GetOwner()->GetActorLocation();
 
-	PhysicsHandle->GrabComponentAtLocation(PushComponent, FName(), OutHit.ImpactPoint);
+	/* Grab object */
+	PhysicsHandle->GrabComponentAtLocationWithRotation(PushComponent, FName(), OutHit.ImpactPoint, PushComponent->GetComponentRotation());
 
 	OnPushObject.Broadcast(true);
 }
